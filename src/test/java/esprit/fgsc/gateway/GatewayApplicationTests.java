77dd4a7b9cd443a54f26633093ae570bfa9daf34
chihabhajji <blackpang23@gmail.com>
2021-10-03 15:00:57 +0100

    Initial commit

---
+++ src/test/java/esprit/fgsc/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.fgsc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
