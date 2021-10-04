22c01d3698de129e450ccf2fcc091b9ef4926581
chihabhajji <blackpang23@gmail.com>
2021-10-04 14:49:24 +0100

    OK BOOMERRRRR

--- src/main/java/esprit/fgsc/gateway/GatewayApplication.java
+++ src/main/java/esprit/fgsc/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package esprit.fgsc.gateway;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -34,8 +35,8 @@ class ServiceInstanceRestController {
         return this.discoveryClient.getInstanceRemoteStatus();
     }
     @RequestMapping("/test")
-    public InstanceInfo servicesUrls(){
-        return discoveryClient.getNextServerFromEureka("FGSC_USERS_MICROSERVICE", false);
+    public List<Application> servicesUrls(){
+        return discoveryClient.getApplications().getRegisteredApplications().get(0);
     }
     public String serviceUrl() {
         InstanceInfo instance = discoveryClient.getNextServerFromEureka("FGSC_USERS_MICROSERVICE", false);
