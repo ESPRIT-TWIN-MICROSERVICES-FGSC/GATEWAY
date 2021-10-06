d1716628fb88813bdeeacbb9ab3ed247a7bd6092
chihabhajji <blackpang23@gmail.com>
2021-10-06 18:32:37 +0100

    Replaced PingUrl with DummyPing in RibbonEurekaClientConfig

--- src/main/java/esprit/fgsc/gateway/config/RibbonEurekaClientConfig.java
+++ src/main/java/esprit/fgsc/gateway/config/RibbonEurekaClientConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class RibbonEurekaClientConfig {
     @Bean
     public IPing ribbonPing(IClientConfig config) {
-        return new PingUrl();
+        return new DummyPing();
     }
     @Bean
     public IRule ribbonRule(IClientConfig config) {
