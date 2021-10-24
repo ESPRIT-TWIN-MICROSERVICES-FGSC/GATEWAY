e302cc30a087b044f0831ca4cb64cbcdb495c7e0
chihabhajji <blackpang23@gmail.com>
2021-10-24 22:43:38 +0100

    silly long shot

--- src/main/java/esprit/fgsc/gateway/GatewayApplication.java
+++ src/main/java/esprit/fgsc/gateway/GatewayApplication.java
@@ -20,7 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @EnableAsync
 @EnableZuulProxy
-@CrossOrigin(origins = "*", value = "*")
+@CrossOrigin("*")
 @RestController
 @EnableEurekaClient
 @SpringBootApplication
