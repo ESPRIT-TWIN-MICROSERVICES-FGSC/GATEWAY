ca423b9b38b467a06fe1f0e206cc99bd7567d788
chihabhajji <blackpang23@gmail.com>
2021-10-24 23:18:12 +0100

    AllowCredentials : false

--- src/main/java/esprit/fgsc/gateway/GatewayApplication.java
+++ src/main/java/esprit/fgsc/gateway/GatewayApplication.java
@@ -46,7 +46,7 @@ public class GatewayApplication {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
