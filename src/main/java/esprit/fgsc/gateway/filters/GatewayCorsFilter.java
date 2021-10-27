df9fc3f5e40e8a013030b39adf614116eec619b0
chihabhajji <blackpang23@gmail.com>
2021-10-27 23:38:21 +0100

    ok

--- src/main/java/esprit/fgsc/gateway/filters/GatewayCorsFilter.java
+++ src/main/java/esprit/fgsc/gateway/filters/GatewayCorsFilter.java
@@ -17,7 +17,7 @@ public class GatewayCorsFilter implements Filter {
 //        System.out.println("CORS FILTER WORKING IN GATEWAY:::");
 //
 //
-        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type,x-requested-with");
         response.setHeader("Access-Control-Max-Age", "3600");
